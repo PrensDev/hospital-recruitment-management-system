74bbf6e0ecabbea7e676aad9ace80b5978738045
Jetsun Prince Torres <59250175+PrensDev@users.noreply.github.com>
2021-10-15 21:48:38 +0800

    Update hospital_db.sql

--- hospital_db.sql
+++ hospital_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2021 at 10:48 AM
+-- Generation Time: Oct 15, 2021 at 03:48 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.23
 
