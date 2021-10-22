947fc67c3c0e0bef0fb32f50e0e5646ab19af348
Jetsun Prince Torres <59250175+PrensDev@users.noreply.github.com>
2021-10-22 20:07:59 +0800

    Additional Updates

--- static/dist/js/pages/home/careers.js
+++ static/dist/js/pages/home/careers.js
@@ -47,7 +47,7 @@ ifSelectorExist('#availableJobList', () => {
 
                     jobList += `
                         <div class="col-md-6 col-12 mb-3">
-                            <div class="card card-primary card-outline h-100">
+                            <div class="card h-100">
                                 <div class="card-body d-flex flex-column justify-content-between h-100">
 
                                     <div>
